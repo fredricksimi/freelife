e6110f7a3c1cce5d0efe334fd3e9a13245316b24
Fredrick Simi <fkemosi@gmail.com>
2021-02-04 12:44:20 +0200

    Second commit

--- FREELIFE/settings.py
+++ FREELIFE/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +27,7 @@ SECRET_KEY = 'yu!hj=gr*zll4z1yq89-9yr0xh@8m%k5+++d4$wbf!sprx*4+l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['freelifeapp.herokuapp.com']
 
 
 # Application definition
@@ -128,3 +130,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn FREELIFE.wsgi --log-file -
